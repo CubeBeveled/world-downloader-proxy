a12fa9580daca9aa4cc310ff928bd4c84e6f304f
CubeBeveled <cubebeveled@gmail.com>
2025-05-19 16:58:52 +0200

    Doesnt download chunks yet but looks good

--- src/index.js
+++ src/index.js
@@ -1,41 +1,71 @@
-const { InstantConnectProxy } = require('prismarine-proxy')
+const protocol = require("minecraft-protocol");
+const color = require("colors");
 
-const targetServer = "6b6t.org";
+const targetServer = "play.6b6t.org";
 const targetPort = 25565;
 const proxyPort = 25566;
 
-const proxy = new InstantConnectProxy({
-  loginHandler: (client) => {
-    return { username: client.username, auth: 'offline' }
-    //return client
-  },
-  clientOptions: {
-    host: targetServer,
-    port: targetPort,
-    version: '1.21',
-    auth: "offline"
-    //auth: "microsoft"
-  },
-  serverOptions: {
-    version: '1.21',
-    port: proxyPort ,
-    "online-mode": false
-  }
-})
+const version = "1.21"
+const cracked = true;
+const proxyMotd = `${targetServer}${targetPort == 25565 ? "" : `:${targetPort}`} [ PROXIED ]`
 
-proxy.on("incoming", (data, meta, toClient, toServer) => {
-  console.log(data)
-  toClient.write(meta.name, data)
-});
+main();
+async function main() {
+  const serverInfo = await getServerInfo(targetServer, targetPort);
 
-proxy.on("outgoing", (data, meta, toClient, toServer) => {
-  toServer.write(meta.name, data)
-});
+  const server = protocol.createServer({
+    port: proxyPort,
+    version: version,
+    "online-mode": !cracked,
+    motd: proxyMotd,
+    maxPlayers: serverInfo.players.max
+  });
 
-proxy.on("end", (username) => {
-  console.log(`${username} ended`)
-});
+  server.on("login", (client) => {
+    console.log(`${color.green("[+]")} ${client.username} joined`)
 
-proxy.on("start", (username) => {
-  console.log(`${username} started`)
-});
+    // open upstream connection *as soon as we know the username*
+    const upstream = protocol.createClient({
+      username: client.username,
+      host: targetServer,
+      port: targetPort,
+      version: version,
+      auth: cracked ? "offline" : "microsoft"
+    });
+
+
+    // client  ->  upstream
+    upstream.on("connect", () => {
+      client.on("packet", (data, meta) => {
+        upstream.write(meta.name, data)
+      })
+    })
+
+    // upstream -> client
+    upstream.on("packet", (data, meta) => {
+      client.write(meta.name, data)
+    })
+
+
+    const close = (reason, log = true) => {
+      if (reason instanceof Error) {
+        reason = reason.message || String(reason);
+      }
+
+      if (log) console.log(`${color.red("[-]")} ${client.username} disconnected (${reason})`)
+
+      if (!client.ended) client.end(reason);
+      if (!upstream.ended) upstream.end(reason);
+    }
+
+    client.on('end', () => close("Disconnected", false))
+    client.on('error', (err) => close(err))
+
+    upstream.on('end', () => close("Proxy error: upstream ended"))
+    upstream.on('error', (err) => close(err))
+  });
+}
+
+async function getServerInfo(ip, port) {
+  return await protocol.ping({ host: ip, port: port });
+}
