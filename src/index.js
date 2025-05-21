3b05a8550cd9aa237ee5a0458e380419a8c664cd
CubeBeveled <cubebeveled@gmail.com>
2025-05-21 19:20:26 +0200

    ayyy shi works better

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 const protocol = require("minecraft-protocol");
 const color = require("colors");
 const fs = require("fs");
+const zlib = require("zlib");
 
-const targetServer = "nihilium.org";
+const targetServer = "play.6b6t.org";
 const targetPort = 25565;
 const proxyPort = 25566;
 
@@ -20,39 +21,47 @@ async function main() {
 
   const serverInfo = await getServerInfo(targetServer, targetPort);
 
-  //const Chunk = prismarineChunk.PCChunk();
-
-  const server = protocol.createServer({
+  const proxy = protocol.createServer({
     port: proxyPort,
     version: version,
     "online-mode": !cracked,
     motd: proxyMotd,
-    maxPlayers: serverInfo.players.max
+    maxPlayers: serverInfo.players.max,
+    //keepAlive: false // Not sure if i should change ts
   });
 
-  server.on("login", (client) => {
-    console.log(`${color.green("[+]")} ${client.username} joined`)
+  proxy.on("listening", () => console.log(color.green(`Proxy listening on port ${proxyPort}`)))
 
-    // open upstream connection *as soon as we know the username*
-    let currentDimension;
-    const upstream = protocol.createClient({
+  proxy.on("login", (client) => {
+    console.log(`${color.green("[+]")} [${color.gray(client.version)}-${client.protocolVersion}] ${client.username} joined`)
+
+    const server = protocol.createClient({
       username: client.username,
       host: targetServer,
       port: targetPort,
       version: version,
-      auth: cracked ? "offline" : "microsoft"
+      auth: cracked ? "offline" : "microsoft",
+      //keepAlive: false // Not sure if i should change ts
     });
+    let currentDimension;
 
-    // client  ->  upstream
-    upstream.on("connect", () => {
-      client.on("packet", (data, meta) => {
-        upstream.write(meta.name, data)
-      })
-    });
+    function close(reason, log = true) {
+      if (log) console.log(`${color.red("[-]")} ${client.username} disconnected (${reason})`)
+
+      if (!client.ended) client.end(reason);
+      if (!server.ended) server.end(reason);
+    }
+
+    client.on("end", (reason) => close(reason, false))
+    client.on("error", (err) => close(err))
+
+    server.on("end", (reason) => close(`Upstream ended: ${reason}`))
+    server.on("error", (err) => close(err))
 
     // upstream -> client
-    upstream.on("packet", (data, meta) => {
-      client.write(meta.name, data)
+    server.on("packet", async (data, meta) => {
+      if (server.state !== protocol.states.PLAY || meta.state !== protocol.states.PLAY || client.ended) return;
+      client.write(meta.name, data);
 
       if (meta.name == "login" && data.worldState?.name) {
         currentDimension = data.worldState.name.replace("minecraft:", "")
@@ -63,33 +72,36 @@ async function main() {
       }
 
       if (meta.name == "map_chunk" && data.chunkData) {
-        fs.writeFileSync(`${worldSaveDir}/${currentDimension}/${data.x}_${data.y}.bin`, data.chunkData)
+        fs.writeFileSync(`${worldSaveDir}/${currentDimension}/${data.x}_${data.z}.bin`, await compress(data.chunkData))
       }
 
-      //if (meta.name == "map_chunk") console.log(data)
-      console.log(meta.name, data)
+      //if (meta.name.includes("map")) console.log(meta.name, data);
+      //if (meta.name.includes("chat")) console.log(meta.name, data);
     });
 
+    // client  ->  upstream
+    client.on("packet", (data, meta) => {
+      if (server.state !== protocol.states.PLAY || meta.state !== protocol.states.PLAY || server.ended) return;
+      server.write(meta.name, data);
+    });
 
-    const close = (reason, log = true) => {
-      if (reason instanceof Error) {
-        reason = reason.message || String(reason);
-      }
-
-      if (log) console.log(`${color.red("[-]")} ${client.username} disconnected (${reason})`)
-
-      if (!client.ended) client.end(reason);
-      if (!upstream.ended) upstream.end(reason);
-    }
-
-    client.on("end", () => close("Disconnected", false))
-    client.on("error", (err) => close(err))
-
-    upstream.on("end", () => close("Proxy error: upstream ended"))
-    upstream.on("error", (err) => close(err))
+    client.on("systemChat", (data) => {
+      console.log(data)
+    });
   });
 }
 
 async function getServerInfo(ip, port) {
   return await protocol.ping({ host: ip, port: port });
+}
+
+function compress(buffer) {
+  return new Promise((resolve, reject) => {
+    zlib.gzip(buffer, (err, compressedBuffer) => {
+      if (err) {
+        console.error("Compression error:", err);
+        reject()
+      } else { resolve(compressedBuffer) }
+    });
+  });
 }
